119d0fc5c7a1fa19e8c9db2a0c7f9a41a5600e00
Glynn Bird <glynn.bird@gmail.com>
2016-07-01 16:50:34 +0100

    added utils.toPromise and tidied

--- index.js
+++ index.js
@@ -1,54 +1,63 @@
 'use strict';
 
-exports.pull= function(remote) {
+var utils = require('pouchdb-utils');
+var PouchDB = require('PouchDB');
+
+
+exports.sayHello = utils.toPromise(function (callback) {
+  //
+  // You can use the following code to
+  // get the pouch or PouchDB objects
+  //
+  // var pouch = this;
+  // var PouchDB = pouch.constructor;
+
+  callback(null, 'hello');
+});
+
+exports.pull = utils.toPromise(function(remote) {
   
-  // keep a reference to this (pouchdb)
+  if (remote.adapter !== 'http') {
+    throw('remote PouchDB must be http');
+  }
   var target = this;
-  var pouch = this;
-  var PouchDB = pouch.constructor;
-  
+
   // create a temporary PouchDB database
-  var tempName = 'envoytemp' + new Date().getTime();
-  var temp = new PouchDB(tempName);
-  
+  var temp = new PouchDB('envoytemp');
   // empty it
   return temp.destroy().then(function() {
     // then recreate it
-    temp = new PouchDB(tempName);
-    
-    // pull all docs from the remote
+    temp = new PouchDB('envoytemp');    
+    // pull all docs from the remote (not the bodies)
     return remote.allDocs();
   }).then(function(response) {
+    // use revsdiff to find difference with local copy
     var diffs = {};
     response.rows.forEach(function(row) {
       diffs[row.id]= [ row.value.rev ];
     });
     return target.revsDiff(diffs);
-    
   }).then(function(response) {
+    // fetch everything about document ids of interest
     var docs = [];
     Object.keys(response).map(function(id) {
       docs.push({id: id});
     });
     return remote.bulkGet({docs: docs, revs:true});
-    
   }).then(function(response) { 
- 
+    // push the changed docs to tempdb
     var docs = [];
     response.results.forEach(function(row) {
       row.docs.forEach(function(d) {
         docs.push(d.ok);
       });
     });
-
     return temp.bulkDocs(docs, {new_edits: false});
   }).then(function() {
     // replicate from temp DB to actual target
     return target.replicate.from(temp);
-  }).catch(function(error) {
-    console.error("pouchdb-envoy error -", error);
   });
-};
+});
 
 /* istanbul ignore next */
 if (typeof window !== 'undefined' && window.PouchDB) {
